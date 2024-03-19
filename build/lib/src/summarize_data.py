d8cff3855f884e6772a53ee62621fe72f86599c1
zachmort <zach.mortenson7@gmail.com>
2024-03-19 18:59:02 -0400

    first edits to python module

--- build/lib/src/summarize_data.py
+++ build/lib/src/summarize_data.py
@@ -6,12 +6,8 @@ import seaborn as sns
 # TODO Probably creating a class here but not sure
 # make function for categorical variables as well (cat=True)
 # mean, median, nulls, distinct answers, total answers, percentiles, 
-def explore_numerics(df: pd.DataFrame, cat=False) -> pd.Series:
-    df = df.select_dtypes(include="np.numeric")
-    count_rows = df.count()
-    sum_columns = df.sum()
-    mean = sum_columns/count_rows
-    return mean
+def explore_numerics(df: pd.DataFrame, cat=False) -> pd.DataFrame:
+    pass
 
 def explore_cats(df) -> pd.DataFrame:
     pass
--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from setuptools import setup, find_packages
 
-VERSION = ""
-DESCRIPTION = ""
+VERSION = "1.0"
+DESCRIPTION = "A function that summarizes data sets passed through it."
 LONG_DESCRIPTION = ""
 
 # Set up
 setup(
-    name=""
+    name="summarizeData"
     , version=VERSION
-    , author=""
+    , author="Zach Mortenson"
     , author_email=""
     , description=DESCRIPTION
-    , packages=find_packages()
-    , install_requires=["numpy", "pandas", "seaborn"]
+    , packages=find_packages(where="explore_data")
+    , install_requires=["numpy", "pandas", "seaborn", "twine"]
+    , license="MIT"
+    , package_dir={"":"explore_data"}
+    , python_requires=">=3.10"
 )
 
